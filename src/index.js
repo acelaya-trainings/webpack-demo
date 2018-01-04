581aa22c62b60e56fec0772519ca2a9823a13e81
Alejandro Celaya <alejandro@alejandrocelaya.com>
2018-01-04 12:17:20 +0100

    Added first assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import _ from 'lodash';
+import './style.css';
+import Alot from './alot.png';
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
+
+    const image = new Image();
+    image.src = Alot;
+    element.appendChild(image);
 
     return element;
 }
